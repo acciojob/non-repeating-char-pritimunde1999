c32e920416e822a99b1732be6a2c86e9d21412a6
Priti Munde <120581202+pritimunde1999@users.noreply.github.com>
2023-07-21 12:02:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+
+
+function nonRepeatingCharacter(s){
+
+	let object = {};
+
+   for(let char of s)
+	{
+		object[char] = (object[char] || 0)+1;
+	}
+
+	for(let char of s)
+		{
+			if(object[char]==1)
+			{
+				return char;
+			}
+		}
+}
+
+const userInput = prompt("Enter a String");
+const result = nonRepeatingCharacter(userInput);
+console.log(result);
